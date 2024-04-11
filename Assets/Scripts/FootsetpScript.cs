af93590431521367f518394fdcae58c5e179752a
Kyler Burson <kyler.burson@gmail.com>
2024-04-10 23:31:51 -0700

    Work on adding Footsteps
    Working on Footsteps but I need the updated Tracker Script

---
+++ Assets/Scripts/FootsetpScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FootsetpScript : MonoBehaviour
+{
+    public GameObject footstep;
+    // Start is called before the first frame update
+    void Start()
+    {
+        footstep.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ()
+    }
+}
