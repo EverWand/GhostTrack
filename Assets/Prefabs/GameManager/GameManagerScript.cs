e31804c2b33ad633c68042aa9233c41bca1495fc
branc-8 <benbranc@uat.edu>
2024-05-01 03:07:25 -0700

    Fix Previous
    asfa

--- Assets/Prefabs/GameManager/GameManagerScript.cs
+++ Assets/Prefabs/GameManager/GameManagerScript.cs
@@ -8,8 +8,8 @@ public class GameManagerScript : MonoBehaviour
     public GameObject Ghost;
     public GameObject Tracker1;
     public GameObject Tracker2;
-    [SerializeField] private Timer = 5.0f;
-    float TimeRemaining = Timer;
+    [SerializeField] private float MaxTimeLeft = 5.0f;
+    float TimeRemaining = 5.0f;
 
     void GameTimer(float DeltaTime)
     {
@@ -24,7 +24,7 @@ public class GameManagerScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         
+        TimeRemaining = MaxTimeLeft;
     }
 
     // Update is called once per frame
