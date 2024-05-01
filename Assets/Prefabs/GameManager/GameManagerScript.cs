975bcb9aea5d590b6503f61e9168d0fd26787197
branc-8 <benbranc@uat.edu>
2024-05-01 02:59:31 -0700

    Timer function added to GameManagerScript.cs
    Yes

--- Assets/Prefabs/GameManager/GameManagerScript.cs
+++ Assets/Prefabs/GameManager/GameManagerScript.cs
@@ -8,17 +8,28 @@ public class GameManagerScript : MonoBehaviour
     public GameObject Ghost;
     public GameObject Tracker1;
     public GameObject Tracker2;
+    [SerializeField] private Timer = 5.0f;
+    float TimeRemaining = Timer;
+
+    void GameTimer(float DeltaTime)
+    {
+        TimeRemaining -= DeltaTime;
+        if (TimeRemaining <= 0)
+        {
+            return;
+        }
+    }
 
 
     // Start is called before the first frame update
     void Start()
     {
-        
+         
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GameTimer(Time.DeltaTime);
     }
 }
